2be9b93f32b150be34e55a2a85dda03241300b02
KaninSukarote <kanin.sukarote@gmail.com>
2016-09-21 00:48:56 +0700

    580610622

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,63 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
+        firstOperand = new BigDecimal(0);
+        secondOperand = new BigDecimal(0);
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = new BigDecimal(operand.operand);
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand = new BigDecimal(operand.operand);
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    public String add() {
+        if(firstOperand.compareTo(BigDecimal.ZERO)<=0 || secondOperand.compareTo(BigDecimal.ZERO)<=0) {
+            throw new RuntimeException("Operand must greater than 0");
+        }
+        return firstOperand.add(secondOperand).stripTrailingZeros().toString();
     }
 
-    public String subtract() throws RuntimeException {
-        return null;
+    public String subtract() {
+        if(firstOperand.compareTo(BigDecimal.ZERO)<=0 || secondOperand.compareTo(BigDecimal.ZERO)<=0){
+            throw  new RuntimeException("Operand must greater than 0");
+        }
+        return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
     }
 
-    public String multiply() throws RuntimeException {
-        return null;
+    public String multiply() {
+        if(firstOperand.compareTo(BigDecimal.ZERO)<=0 || secondOperand.compareTo(BigDecimal.ZERO)<=0){
+            throw  new RuntimeException("Operand must greater than 0");
+        }
+        return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(secondOperand.equals(BigDecimal.ZERO)){
+            throw new ArithmeticException();
+        }
+        if(firstOperand.compareTo(BigDecimal.ZERO)<=0 || secondOperand.compareTo(BigDecimal.ZERO)<=0){
+            throw  new RuntimeException("Operand must greater than 0");
+        }
+        return firstOperand.divide(secondOperand,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(firstOperand.compareTo(BigDecimal.ZERO)<=0 || secondOperand.compareTo(BigDecimal.ZERO)<=0){
+            throw  new RuntimeException("Operand must greater than 0");
+        }
+        return firstOperand.pow(secondOperand.intValue()).stripTrailingZeros().toString();
     }
 
 }
