c5758ecaaff810f1a4a55fddcb229fca6b5da7f7
Thanapat Sornsrivichai <cococrunch9@hotmail.com>
2016-09-23 14:58:56 +0700

    v1

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -2,38 +2,125 @@ package cpe200;
 
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        this.firstOperand = operand;
     }
 
 
     public void setSecondOperand(Operand operand) {
+        this.secondOperand = operand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        double num1,num2,hold;
+        String ans;
+        num1 = Double.parseDouble(firstOperand.getOperand());
+        num2 = Double.parseDouble(secondOperand.getOperand());
+        if(num1 < 0 || num2 < 0)
+        {
+            throw new RuntimeException();
+        }
+        hold = num1 + num2;
+        hold = (double)Math.round(hold * 100000d) / 100000d;
+        if(hold*10%10 == 0)
+        {
+            int hold2 = ((int) hold);
+            ans = Integer.toString(hold2);
+        }
+        else ans = Double.toString(hold);
+        return ans;
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        double num1,num2,hold;
+        String ans;
+        num1 = Double.parseDouble(firstOperand.getOperand());
+        num2 = Double.parseDouble(secondOperand.getOperand());
+        if(num1 < 0 || num2 < 0)
+        {
+            throw new RuntimeException();
+        }
+        hold = num1 - num2;
+        hold = (double)Math.round(hold * 100000d) / 100000d;
+        if(hold*10%10 == 0)
+        {
+            int hold2 = ((int) hold);
+            ans = Integer.toString(hold2);
+        }
+        else ans = Double.toString(hold);
+        return ans;
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        double num1,num2,hold;
+        String ans;
+        num1 = Double.parseDouble(firstOperand.getOperand());
+        num2 = Double.parseDouble(secondOperand.getOperand());
+        if(num1 < 0 || num2 < 0)
+        {
+            throw new RuntimeException();
+        }
+        hold = num1 * num2;
+        hold = (double)Math.round(hold * 100000d) / 100000d;
+        if(hold*10%10 == 0)
+        {
+            int hold2 = ((int) hold);
+            ans = Integer.toString(hold2);
+        }
+        else ans = Double.toString(hold);
+        return ans;
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        double num1,num2,hold;
+        String ans;
+        num1 = Double.parseDouble(firstOperand.getOperand());
+        num2 = Double.parseDouble(secondOperand.getOperand());
+        if(num1 < 0 || num2 < 0)
+        {
+            throw new RuntimeException();
+        }
+
+        if(num2 == 0)
+        {
+            throw new ArithmeticException();
+        }
+        hold = num1/num2;
+        hold = (double)Math.round(hold * 100000d) / 100000d;
+        if(hold*10%10 == 0)
+        {
+            int hold2 = ((int) hold);
+            ans = Integer.toString(hold2);
+        }
+        else ans = Double.toString(hold);
+        return ans;
     }
 
     public String power() throws RuntimeException {
-        return null;
+        double num1,num2,hold;
+        String ans;
+        num1 = Double.parseDouble(firstOperand.getOperand());
+        num2 = Double.parseDouble(secondOperand.getOperand());
+        if(num1 < 0 || num2 < 0)
+        {
+            throw new RuntimeException();
+        }
+        hold = Math.pow(num1,num2);
+        hold = (double)Math.round(hold * 100000d) / 100000d;
+        if(hold*10%10 == 0)
+        {
+            int hold2 = ((int) hold);
+            ans = Integer.toString(hold2);
+        }
+        else ans = Double.toString(hold);
+        return ans;
     }
 
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,46 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    protected String userName;
+    protected String password;
+    protected String hold;
 
     public User() {
+        this.userName = getUserName();
+        this.password = getPassword();
     }
 
-    public String setUserName(String name) {
-        return null;
+    public String setUserName(String name) throws RuntimeException {
+        hold = userName;
+        this.userName = name;
+        String pattern = "[a-zA-Z][a-zA-Z0-9]+";
+        if(name.matches(pattern) && name.length() >= 8)
+        {
+            userName = name;
+            return hold;
+        }
+        else
+            {
+            throw new RuntimeException();
+        }
     }
 
     public int setPassword(String name) {
-        return 0;
+        this.password = name;
+        String pattern = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]+";
+        if (name.matches(pattern) && name.length() >= 12)
+        {
+            return name.length();
+        } else
+            throw new RuntimeException();
     }
 
     public String getUserName() {
 
-        return null;
+        return this.userName;
     }
 
     public String getPassword() {
-        return null;
+        return this.password;
     }
 }
